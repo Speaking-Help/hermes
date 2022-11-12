88dcf6247e6a8756c1eea70b668c203b2600e973
par5ul1 <elexir0@gmail.com>
2022-11-11 18:10:52 -0800

    added delta functionality to GEC

--- src/modules/grammar_corrector.py
+++ src/modules/grammar_corrector.py
@@ -2,6 +2,8 @@ from gramformer import Gramformer
 import torch
 import spacy
 
+import difflib
+
 def set_seed(seed):
     """
     Sets the seed for PyTorch's RNG
@@ -26,6 +28,58 @@ class Grammar_Corrector:
         corrected_sentences = self._gf.correct(sentence, max_candidates=1)
         return corrected_sentences.pop()    #only one value is present, pop it from set
 
+    def find_deltas(self, sentence, corrected):
+        """
+        Takes in the original sentence and the corrected sentence
+
+        Returns an array of dictionaries indicating the type of change, starting index and ending index (not-inclusive) of non-punctuation changes in the corrected sentence from the original sentence.
+
+        Example:
+        `find_delta("this is the original sentence", "This is the corrected sentence.")`
+        returns `[{'type': 'ADJ', 'start': 12, 'end': 21}]`       
+        """
+        deltas = []
+        edits = self._gf.get_edits(sentence, corrected)
+
+        # Only show edits that are not about punctuation
+        edits = [edit for edit in edits if (edit[0] != "NOUN" and edit[0] != "ORTH" and edit[0] != "SPELL" and edit[0] != "OTHER")]
+        for edit in edits:
+          pos = self._character_position_from_word_position(corrected.split(" "), edit[5], edit[6])
+          deltas.append({
+            "type": edit[0],
+            "start": pos[0],
+            "end": pos[1]
+          })
+
+        return deltas
+
+    @staticmethod
+    def _character_position_from_word_position(split_sentence, start, end):
+      """
+      Takes in a list of words (assumed to only be separated by one space character), a starting word position, and an ending word position (not-inclusive).
+
+      Returns the start and end positions of the segment of word(s) in the original sentence.
+
+      Example:
+      `character_position_from_word_position(["This", "is", "a", "sentence"], 2, 4)`
+      returns `(8, 18)` 
+      """
+
+      # start off with the number of spaces
+      char_start = start 
+      char_end = end-start
+
+      for i in range(start):
+        char_start += len(split_sentence[i])
+
+      for i in range(end-start):
+        char_end += len(split_sentence[start+i])
+      char_end += char_start - 1
+
+      return (char_start, char_end)
+      
+
+
 
 def main():
     """
@@ -33,8 +87,21 @@ def main():
     """
     set_seed(5279)
     gc = Grammar_Corrector()
-    sentence = gc.correct_sentence("Thsi sentence is incorrect. it has bad spelling missing commas no period and other issues")
+    sentence = "Thsi sentence am incorrect. it's has bads spelling missing commas no period and other issues"
+    corrected = gc.correct_sentence(sentence)
     print(sentence)
+    print(corrected)
+
+    deltas = gc.find_deltas(sentence, corrected)
+    # print(deltas) # Can be printed for debugging
+    correction_underline = [" "]*len(corrected)
+    for delta in deltas:
+      for i in range(delta["end"]-delta["start"]):
+          correction_underline[delta["start"]+i] = "^"
+
+    correction_underline = "".join(correction_underline)
+    print(correction_underline)
+
 
 if __name__ == "__main__":
     main()
